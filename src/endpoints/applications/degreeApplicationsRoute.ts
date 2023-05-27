730afe42cd6ddb481ff5166ca23cd3d4286d53ff
Joey Prüssing <j.pruessing28@gmail.com>
2023-05-27 20:54:32 +0200

    put and delete work not for applications. applications get cleaned. check if applicant exists and if course exists when posting application.

--- src/endpoints/applications/degreeApplicationsRoute.ts
+++ src/endpoints/applications/degreeApplicationsRoute.ts
@@ -4,6 +4,7 @@ import { cleanUser } from '../user/userRouteStrict'
 import { cleanCourse } from '../degreeCourses/degreeRoute'
 import * as applicationServices from './degreeApplicationService'
 import * as courseServices from '../degreeCourses/degreeService'
+import * as userServices from '../user/userService'
 
 
 const router = express.Router()
@@ -16,9 +17,16 @@ router.get('/', isAuthorized, isAdmin, async (req: express.Request, res: any) =>
     console.log('Getting applications.')
     console.log(req.query)
 
-    const applications = await applicationServices.getManyApplications(req.query)
+    try {
+        const applications = await applicationServices.getManyApplications(req.query)
+        // if (!applications) return res.json({ Error: 'Something went wrong.'})
+        console.log(applications)
+        res.json(cleanApplication(applications as Record<any, any>[]))
+    } catch (error) {
+        res.json({ Error: error })
+    }
 
-    res.send(applications)
+    // res.json(applications)
 
 
     // const applications = await applicationServices.getAllApplications()
@@ -31,32 +39,40 @@ router.get('/myApplications', isAuthorized, async (req: any, res: any) => {
     const userID = res.decodedUser.userID
     console.log(res.decodedUser)
     const applications = await applicationServices.getManyApplications({ applicantUserID: userID })
-    res.json(applications)
+    res.json(cleanApplication(applications as Record<any, any>[]))
 })
 
 router.post('/', isAuthorized, async (req: any, res: any) => {
     console.log('posting one.')
     const isAdmin = res.decodedUser.isAdministrator
-    // console.log(res.decodedUser)
 
     let applicant: string
     if (isAdmin) {
         if ('applicantUserID' in req.body) {
+            // admin posting a user
             applicant = req.body.applicantUserID
+            try {
+                const user = await userServices.getOneUser(applicant)
+                if (!user) return res.status(404).json({ Error: 'Applicant not found.' })
+            } catch (error) {
+                res.json({ Error: error })
+            }
         } else {
+            // admin posting self
             applicant = res.decodedUser.userID
         }
     } else {
+        // user posting self
         applicant = res.decodedUser.userID
     }
 
     // check if course exists
     try {
         const course = await courseServices.getOneCourse(req.body.degreeCourseID)
+        if (!course) return res.status(404).json({ Error: 'Course not found.'})
     } catch (error) {
         return res.status(404).json({ Error: 'Course not found.'})
     }
-    // if (!course) 
 
     // check if applicant has existing application for given Course.
     const existingCourses = await applicationServices.getManyApplications({
@@ -73,8 +89,52 @@ router.post('/', isAuthorized, async (req: any, res: any) => {
         targetPeriodYear: req.body.targetPeriodYear,
         targetPeriodShortName: req.body.targetPeriodShortName
     })
-    application.save()
-    res.send(application)
+
+    res.status(200).json(cleanApplication(application))
 })
+
+router.put('/:id', async (req: express.Request, res: express.Response) => {
+    console.log('putting one')
+    try {
+        const application = await applicationServices.updateOneApplication(req.params.id, req.body)
+        if (!application) res.json({ Error: 'Not Found.'})
+        res.json(cleanApplication(application as Record<any, any>))
+    } catch (error) {
+        res.json({ Error: error })
+    }
+})
+
+router.delete('/:id', async (req: express.Request, res: express.Response) => {
+    console.log('delete one')
+    try {
+        const application = await applicationServices.deleteOneApplication(req.params.id)
+        res.sendStatus(204)
+    } catch (error) {
+        res.json({ Error: error })
+    }
+})
+
+export function cleanApplication(application: Record<any, any> | Record<any, any>[]): object | object[] {
+    if (Array.isArray(application)) {
+        const applications = application
+        const cleanApplications: Record<any, any>[] = applications.map((application) => ({
+            applicantUserID: application.applicantUserID,
+            degreeCourseID: application.degreeCourseID,
+            targetPeriodYear: application.targetPeriodYear,
+            targetPeriodShortName: application.targetPeriodShortName,
+            id: application._id
+        }))
+        return cleanApplications
+    } else {
+        const cleanApplication: Record<any, any> = {
+            applicantUserID: application.applicantUserID,
+            degreeCourseID: application.degreeCourseID,
+            targetPeriodYear: application.targetPeriodYear,
+            targetPeriodShortName: application.targetPeriodShortName,
+            id: application._id
+        }
+        return cleanApplication
+    }
+}
 
 export default router
