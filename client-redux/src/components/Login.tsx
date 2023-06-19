cbff62d2ec245b9f53f9cade8a9337ae010e712c
Joey Prüssing <j.pruessing28@gmail.com>
2023-06-19 16:19:50 +0200

    bootstrap css now imported. all id set correctly afaik.

--- client-redux/src/components/Login.tsx
+++ client-redux/src/components/Login.tsx
@@ -1,7 +1,10 @@
-import React from "react"
+import React, { MouseEventHandler } from "react"
 import { useDispatch } from "react-redux"
 import { useAppSelector } from "../app/hooks"
 import { clearToken, selectAuth, setToken } from "../features/authSlice"
+import Form from "react-bootstrap/Form"
+import Modal from "react-bootstrap/Modal"
+import Button from "react-bootstrap/Button"
 
 // interface LoginProps {
 //   setLoggedIn: React.Dispatch<React.SetStateAction<boolean>>
@@ -10,58 +13,198 @@ import { clearToken, selectAuth, setToken } from "../features/authSlice"
 export default function Login() {
   const isAuth = useAppSelector(selectAuth).isAuthenticated
   const dispatch = useDispatch()
+
+  const [showLogin, setShowLogin] = React.useState(false)
   return (
     <div id="LoginDialog">
       {!isAuth && (
-        <form
-          onSubmit={(e) => {
-            e.preventDefault()
-            const username = (
-              document.getElementById(
-                "LoginDialogUserIDText",
-              ) as HTMLInputElement
-            ).value
-            const password = (
-              document.getElementById(
-                "LoginDialogPasswordText",
-              ) as HTMLInputElement
-            ).value
-            fetch("https://localhost/api/authenticate", {
-              method: "GET",
-              headers: {
-                "Content-Type": "application/json",
-                Authorization: `Basic ${btoa(`${username}:${password}`)}`,
-              },
-            }).then((response) => {
-              console.log("Res:", response)
-              if (response.status === 200) {
-                const token: string = response.headers
-                  .get("Authorization")
-                  ?.split(" ")[1] as string
-                dispatch(setToken(token))
-                console.log("Log in successful")
-              } else {
-                console.log("Log in failed")
-              }
-            })
-          }}
-        >
-          <label htmlFor="username">Username</label>
-          <input type="text" name="username" id="LoginDialogUserIDText" />
-          <br />
-          <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="LoginDialogPasswordText" />
-          <br />
-          <br />
-          <button id="PerformLoginButton" type="submit">
-            Login
-          </button>
-        </form>
+        <>
+          {!showLogin && (
+            <Button
+              id="OpenLoginDialogButton"
+              onClick={() => setShowLogin(true)}
+            >
+              Show dialogue
+            </Button>
+          )}
+          <Modal show={showLogin}>
+            <Modal.Dialog>
+              <Modal.Header>
+                <Modal.Title>Login Dialogue</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                {/* <form
+                  onSubmit={(e) => {
+                    e.preventDefault()
+                    const username = (
+                      document.getElementById(
+                        "LoginDialogUserIDText",
+                      ) as HTMLInputElement
+                    ).value
+                    const password = (
+                      document.getElementById(
+                        "LoginDialogPasswordText",
+                      ) as HTMLInputElement
+                    ).value
+                    fetch("https://localhost/api/authenticate", {
+                      method: "GET",
+                      headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Basic ${btoa(
+                          `${username}:${password}`,
+                        )}`,
+                      },
+                    }).then((response) => {
+                      console.log("Res:", response)
+                      if (response.status === 200) {
+                        const token: string = response.headers
+                          .get("Authorization")
+                          ?.split(" ")[1] as string
+                        dispatch(setToken(token))
+                        console.log("Log in successful")
+                      } else {
+                        console.log("Log in failed")
+                      }
+                    })
+                  }}
+                >
+                  <label htmlFor="username">Username</label>
+                  <input
+                    type="text"
+                    name="username"
+                    id="LoginDialogUserIDText"
+                  />
+                  <br />
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="LoginDialogPasswordText"
+                  />
+                  <br />
+                  <br />
+                  <Button id="PerformLoginButton" type="submit">
+                    Login
+                  </Button>
+                </form> */}
+                <Form
+                  onSubmit={(e) => {
+                    e.preventDefault()
+                    const username = (
+                      document.getElementById(
+                        "LoginDialogUserIDText",
+                      ) as HTMLInputElement
+                    ).value
+                    const password = (
+                      document.getElementById(
+                        "LoginDialogPasswordText",
+                      ) as HTMLInputElement
+                    ).value
+                    fetch("https://localhost/api/authenticate", {
+                      method: "GET",
+                      headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Basic ${btoa(
+                          `${username}:${password}`,
+                        )}`,
+                      },
+                    }).then((response) => {
+                      console.log("Res:", response)
+                      if (response.status === 200) {
+                        const token: string = response.headers
+                          .get("Authorization")
+                          ?.split(" ")[1] as string
+                        dispatch(setToken(token))
+                        console.log("Log in successful")
+                      } else {
+                        console.log("Log in failed")
+                      }
+                    })
+                  }}
+                >
+                  <Form.Group>
+                    <Form.Label>Username:</Form.Label>
+                    <Form.Control
+                      id="LoginDialogUserIDText"
+                      type="text"
+                      placeholder="Your username"
+                    />
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Label>Password:</Form.Label>
+                    <Form.Control
+                      id="LoginDialogPasswordText"
+                      type="password"
+                      placeholder=""
+                    />
+                  </Form.Group>
+                  <Button
+                    id="PerformLoginButton"
+                    variant="primary"
+                    type="submit"
+                  >
+                    Log in
+                  </Button>
+                </Form>
+              </Modal.Body>
+              <Modal.Footer>
+                {/* <Button variant="primary">Save changes</Button> */}
+                <Button onClick={() => setShowLogin(false)} variant="secondary">
+                  Close
+                </Button>
+              </Modal.Footer>
+            </Modal.Dialog>
+          </Modal>
+        </>
+        // <>
+        //   {!showLogin && (
+        //     <Button onClick={setShowLogin(true) as any}>Logo</Button>
+        //   )}
+
+        //   <Modal show={showLogin}>
+        //     <Modal.Dialog>
+        //       <Modal.Header>
+        //         <Modal.Title>Login Window</Modal.Title>
+        //       </Modal.Header>
+        //       <Modal.Body>
+        // <Form>
+        //   <Form.Group>
+        //     <Form.Label>Username:</Form.Label>
+        //     <Form.Control
+        //       id="LoginDialogUserIDText"
+        //       type="text"
+        //       placeholder="Your username"
+        //     />
+        //   </Form.Group>
+        //   <Form.Group>
+        //     <Form.Label>Password:</Form.Label>
+        //     <Form.Control
+        //       id="LoginDialogPasswordText"
+        //       type="password"
+        //       placeholder=""
+        //     />
+        //   </Form.Group>
+        //   <Button
+        //     id="PerformLoginButton"
+        //     variant="primary"
+        //     type="submit"
+        //   >
+        //     Log in
+        //   </Button>
+        // </Form>
+        //       </Modal.Body>
+        //       <Modal.Footer>
+        //         {/* <Button variant="primary">Save changes</Button>
+        //     <Button variant="secondary">Close</Button> */}
+        //       </Modal.Footer>
+        //     </Modal.Dialog>
+        //   </Modal>
+        // </>
       )}
       {isAuth && (
-        <button id="LogoutButton" onClick={() => dispatch(clearToken())}>
+        <Button id="LogoutButton" onClick={() => dispatch(clearToken())}>
           Logout
-        </button>
+        </Button>
       )}
     </div>
   )
--- client-redux/src/features/authSlice.ts
+++ client-redux/src/features/authSlice.ts
@@ -11,23 +11,6 @@ const initialState: AuthState = {
   isAuthenticated: false,
 }
 
-// export const setTokenAsync = createAsyncThunk(
-//   "auth/setTokenAsync",
-//   async (token: string, { dispatch }) => {
-//     const encodedExpiresAt = token.split(".")[1]
-//     const expiresAt = JSON.parse(atob(encodedExpiresAt))
-//     const currentTimestamp = Math.floor(Date.now() / 1000)
-//     const timeLeft = (expiresAt.exp - currentTimestamp) * 1000
-//     console.log(timeLeft)
-
-//     // Wait for the token to expire
-//     await new Promise((resolve) => setTimeout(resolve, timeLeft))
-
-//     // Dispatch the clearToken action
-//     dispatch(clearToken())
-//   },
-// )
-
 const authSlice = createSlice({
   name: "auth",
   initialState,
